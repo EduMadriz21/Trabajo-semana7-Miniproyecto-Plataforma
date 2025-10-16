fb0abcd30d072e33dbda6747bc23b3546e38dfb1
EduMadriz20 <epmadriz@gmail.com>
2025-10-16 14:08:06 -0600

    Indique las clases de cada tarea

---
+++ ListarTareas/ListarTareas/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ListarTareas
+{
+    // Clase que representa una tarea
+    public class Tarea
+    {
+        public string Descripcion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public bool Completada { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
