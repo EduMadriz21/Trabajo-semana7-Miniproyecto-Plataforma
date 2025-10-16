4c35f22def1f8de6ae51f6a2c54b17d350678ba4
Naza2006 <nazario@uamv.edu.ni>
2025-10-16 14:13:15 -0600

    Cree la clase para representar una tarea

---
+++ agregar_tareas/agregar_tareas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace agregar_tareas
+{
+    // Clase que representa una tarea
+    public class Tarea
+    {
+        public string Descripcion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public bool Completada { get; set; }
+    }
+
+    
+}
