1ea7877dd51826025fd038e20e9cf195c7d6f0c4
Joshua <Joshua@DESKTOP-Q6G780M>
2025-10-16 19:24:56 -0600

    Agregué método CompleteTask y DeleteTask en AdministradorDeTareas

---
+++ TaskManagerProject/TaskManagerProject/AdministradorDeTareas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskManagerProject
+{
+    public class AdministradorDeTareas //comentario para probar git
+    {
+        private List<Tarea> tareas = new List<Tarea>();
+
+        public void AddTask(Tarea tarea)
+        {
+            tareas.Add(tarea);
+        }
+
+        public bool CompleteTask(int id)
+        {
+            var tarea = tareas.Find(t => t.Id == id);
+            if (tarea != null)
+            {
+                tarea.IsCompleted = true;
+                return true;
+            }
+            return false;
+        }
+
+        public void ListTasks()
+        {
+            foreach (var tarea in tareas)
+            {
+                Console.WriteLine($"ID: {tarea.Id}, Desc: {tarea.Description}, Completada: {tarea.IsCompleted}");
+            }
+        }
+    }
+
+}
+
